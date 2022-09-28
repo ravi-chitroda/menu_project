28943b9de15533e5b26d6cf647c7a82abc807cbd
Ravi Chitroda <ravi.chitroda91@gmail.com>
2022-09-28 12:32:18 +0530

    Required setup files

---
+++ src/Catagories.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Catagories = () => {
+  return (
+    <div>
+      <h2>Category Page</h2>
+    </div>
+  );
+};
+
+export default Catagories;
---
+++ src/Menu.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Menu = () => {
+  return (
+    <div>
+      <h2>Menu </h2>
+    </div>
+  );
+};
+export default Menu;
